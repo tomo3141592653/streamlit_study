9fbc1c28a47d06ebc7134ebbd2d90e989d508bdd
tomo3141592653 <tomo3141592653@gmail.com>
2024-02-07 01:25:07 +0900

    add file io

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from sympy import factorint
+import io 
 
 # Streamlit アプリのタイトル
 st.title('素因数分解計算機')
@@ -17,3 +18,30 @@ if st.button('素因数分解'):
 
     # 結果を表示
     st.write(f'{number} の素因数分解: {result}')
+
+
+# ファイルアップロードセクション
+uploaded_file = st.file_uploader("数字のリストが含まれるテキストファイルをアップロードしてください (.txt)")
+
+if uploaded_file is not None:
+    # ファイルの内容を読み込む
+    content = uploaded_file.getvalue().decode("utf-8")
+    numbers = content.splitlines()
+
+    # 処理結果を保存するための文字列IO
+    result_io = io.StringIO()
+
+    # ファイルから読み取った各行に対して処理
+    for number in numbers:
+        number = int(number)
+        factors = factorint(number)
+        result = ', '.join([f'{factor}^{exponent}' if exponent > 1 else str(factor) for factor, exponent in factors.items()])
+        result_io.write(f'{number} の素因数分解: {result}\n')
+    
+    # ユーザーにダウンロードリンクを提供
+    st.download_button(
+        label="結果をダウンロード",
+        data=result_io.getvalue(),
+        file_name="factorization_results.txt",
+        mime="text/plain"
+    )
